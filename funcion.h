0a02667fc018933b9bffb6fdab389ab39cd3e57c
francisco uribe <fjuribe@ing.ucsc.cl>
2018-08-25 19:25:43 -0300

    modificacion de la funcion calculadora horoscopo

--- funcion.h
+++ funcion.h
@@ -1,108 +1,152 @@
-<<<<<<< HEAD
+
 /*
 francisco uribe
 pablo peña
 herman fernandez
 */
-=======
 
->>>>>>> ee6f2f3c7c69a753d5122087d8487fcfca6f7edd
 //primer servicio del sistema hóroscopo
 void calculadora_horoscopo(unsigned d, unsigned m,unsigned a){
 	
-	if(d>=20 && m==1){
+			/*************acuario ***********/
+	  if(d>=20 && m==1){
+	       	//Enero 20 – Febrero 18-> acuario
+	        //printf("sus signo es acuario");
+    	}
 		if(d<=18 && m==2){
 			//Enero 20 – Febrero 18-> acuario
 	        //printf("sus signo es acuario");
 		}
-	}
+		
+		
+		
+		/*************piscis ***********/
 		if(d>=19 && m==2){
-	     	if(d<=20 && m==3){
+	     	//Febrero 19 – Marzo 20
+	        //printf("sus signo es piscis");
+    	}
+	    
+	    if(d<=20 && m==3){
 			//Febrero 19 – Marzo 20
 	        //printf("sus signo es piscis");
 		}
-	}
-	
+		
+		
+		/************* aries ***********/
 		if(d>=21 && m==3){
-		      if(d<=19 && m==4){
+		      //Marzo 21 –Abril 19
+	            //printf("sus signo es aries");
+      	}
+	    if(d<=19 && m==4){
 		    	//Marzo 21 –Abril 19
 	            //printf("sus signo es aries");
 		}
-	}
 	
+	
+	   		/************* tauro ***********/
 		 if(d>=20 && m==4){
-	          if(d<=20 && m==5){
+	          //Abril 20 – Mayo 20-> tauro
+	          //printf("sus signo es tauro");
+	    }
+	    if(d<=20 && m==5){
 		        //Abril 20 – Mayo 20-> tauro
 	            //printf("sus signo es tauro");
 		}
-	}
-	
+		
+		/************* geminis ***********/
 		if(d>=21 && m==5){
-	        	if(d<=20 && m==6){
+	        		//Mayo 21 – Junio 20 -> geminis
+	                //printf("sus signo es geminis");
+     	}
+	    if(d<=20 && m==6){
 		        	//Mayo 21 – Junio 20 -> geminis
 	                //printf("sus signo es geminis");
 		}
-	}
-	
 
-		if(d>=20 && m==6){
-		        if(d<=18 && m==7){
-		         	//Enero 20 – Febrero 18-> acuario
-	                //printf("sus signo es acuario");
+
+       	/************* cancer ***********/
+		if(d>=21 && m==6){
+		        	//Junio 21 – Julio 22-> cancer
+	                //printf("sus signo es cancer");   
+    	}	
+    	if(d<=22 && m==7){
+		         	//Junio 21 – Julio 22-> cancer
+	                //printf("sus signo es cancer");  
 		}
-	}	
-	        if(d>=20 && m==1){
-		          if(d<=18 && m==2){
-	           		//Enero 20 – Febrero 18-> acuario
-	                //printf("sus signo es acuario");
+		
+		/*************leo ***********/
+	        if(d>=23 && m==7){
+		            //Julio 23–Agosto 22-> leo
+	                //printf("sus signo es leo"); 
+    	}
+	    if(d<=22 && m==8){
+	           		  //Julio 23–Agosto 22-> leo
+	                //printf("sus signo es leo"); 
 		}
-	}
 	
+	
+	   /************* virgo **********************/
+		if(d>=23 && m==8){
+		       //Agosto 23 – Septiembre 22-> virgo
+	           //printf("sus signo es virgo");
+    	}
+    	if(d<=22 && m==9){
+			//Agosto 23 – Septiembre 22-> virgo
+	           //printf("sus signo es virgo");
+		}
+		
+		
+		
+		/************ libra ************/
+		if(d>=23 && m==9){
+		    //Septiembre 23 – Octubre 22-> libra
+	        //printf("sus signo es libra");
+	    }
+	     if(d<=22 && m==10){
+		   //Septiembre 23 – Octubre 22-> libra
+	        //printf("sus signo es libra");
+		}
+	
+	/****************** escorpion ********************/
+		if(d>=23 && m==10){
+		    //Octubre 23 – Noviembre 21-> escorpion
+	        //printf("sus signo es escorpion");
+	    }
+	     if(d<=21 && m==11){
+		  //Octubre 23 – Noviembre 21-> escorpion
+	        //printf("sus signo es escorpion");
+		}
+		
+		/*****************sagitario**********************/
+		if(d>=22 && m==11){
+		   	//Noviembre 22 - Diciembre 21-> sagitario
+	        //printf("sus signo es sagitario");
+     	}
+     	if(d<=22 && m==12){
+			//Noviembre 22 - Diciembre 21-> sagitario
+	        //printf("sus signo es sagitario");
+		}
+	   
+	    /********************* sagitario ***********************/
 		if(d>=20 && m==1){
-		if(d<=18 && m==2){
+		  //Enero 20 – Febrero 18-> acuario
+	      //printf("sus signo es acuario");
+	    }
+	     if(d<=18 && m==2){
 			//Enero 20 – Febrero 18-> acuario
 	        //printf("sus signo es acuario");
 		}
-	}
 	
-		if(d>=20 && m==1){
-		if(d<=18 && m==2){
-			//Enero 20 – Febrero 18-> acuario
-	        //printf("sus signo es acuario");
+	 /**************** capricornio ******************/
+		if(d>=22 && m==12){
+		    //Diciembre 22 – Enero 19-> capricornio
+	        //printf("sus signo es capricornio");
+	    }
+	    if(d<=19 && m==1){
+			 //Diciembre 22 – Enero 19-> capricornio
+	        //printf("sus signo es capricornio");
 		}
-	}
-	
-		if(d>=20 && m==1){
-		if(d<=18 && m==2){
-			//Enero 20 – Febrero 18-> acuario
-	        //printf("sus signo es acuario");
-		}
-	}
-		if(d>=20 && m==1){
-		if(d<=18 && m==2){
-			//Enero 20 – Febrero 18-> acuario
-	        //printf("sus signo es acuario");
-		}
-	}
-	
-		if(d>=20 && m==1){
-		if(d<=18 && m==2){
-			//Enero 20 – Febrero 18-> acuario
-	        //printf("sus signo es acuario");
-		}
-	}
-	
-		if(d>=20 && m==1){
-		if(d<=18 && m==2){
-			//Enero 20 – Febrero 18-> acuario
-	        //printf("sus signo es acuario");
-		}
-	}
-	
-	
-	else if(d==19){
-		//Febrero 19 – Marzo 20 ->piscis
-	}
+
 }
 
 //segundo servicio del sistema hóroscopo
