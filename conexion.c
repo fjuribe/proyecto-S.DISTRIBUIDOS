61f8783f05f31bd2172ae0a8f449c25af324c0d4
francisco uribe <fjuribeui@gmail.com>
2018-08-26 16:01:46 -0300

    agregando conexion

--- conexion.c
+++ conexion.c
@@ -1,5 +1,42 @@
-/*
-francisco uribe
-pablo pe馻
-herman fernandez
-*/
+/* librer铆as que usaremos */
+#include <mysql/mysql.h> /* libreria que nos permite hacer el uso de las conexiones y consultas con MySQL */
+#include <stdio.h> /* Para poder usar printf, etc. */
+
+int main(int argc, char const *argv[])
+{
+	MYSQL *conn; /* variable de conexi贸n para MySQL */
+	MYSQL_RES *res; /* variable que contendra el resultado de la consuta */
+	MYSQL_ROW row; /* variable que contendra los campos por cada registro consultado */
+	char *server = "localhost"; /*direccion del servidor 127.0.0.1, localhost o direccion ip */
+	char *user = "root"; /*usuario para consultar la base de datos */
+	char *password = "root"; /* contrase帽a para el usuario en cuestion */
+	char *database = "prueba"; /*nombre de la base de datos a consultar */
+	conn = mysql_init(NULL); /*inicializacion a nula la conexi贸n */
+
+	/* conectar a la base de datos */
+	if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
+	{ /* definir los par谩metros de la conexi贸n antes establecidos */
+		fprintf(stderr, "%s\n", mysql_error(conn)); /* si hay un error definir cual fue dicho error */
+		exit(1);
+	}
+
+	/* enviar consulta SQL */
+	if (mysql_query(conn, "select * from datos"))
+	{ /* definicion de la consulta y el origen de la conexion */
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+    
+     //obtiene los resultados
+	res = mysql_use_result(conn);
+	printf("ID\tNombre\t\tedad\n");
+	
+	/*ciclo para recorrer la variables res con todos los registros obtenidos para su uso*/
+    while((row=mysql_fetch_row(res))!=NULL)
+	while ((row = mysql_fetch_row(res)) != NULL) /* recorrer la variable res con todos los registros obtenidos para su uso */
+		printf("%s\t%s\t%s \n", row[0],row[1],row[2]); /* la variable row se convierte en un arreglo por el numero de campos que hay en la tabla */
+
+	/* se libera la variable res y se cierra la conexi贸n */
+	mysql_free_result(res);
+	mysql_close(conn);
+}
